377911f707f03b40876abb18641714232865810d
opajkovic <oljaolja170@gmail.com>
2022-04-12 12:22:09 +0200

    added ex01

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,10 +1,10 @@
 class Car {
     constructor(manufacturer, model, production) {
-    this.manufacturer = manufacturer;
-    this.model = model;
-    this.production = production;
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.production = production;
     }
-    }
+}
 const bmw = new Car("BMW", "BMW X3", 2003);
 console.log(bmw.manufacturer);
 console.log(bmw.model);
